b2d706fa7827ee72e7a23c93f0865f9bc3a58f94
Nik Martinez <nikolaus.c.martinez@gmail.com>
2017-10-04 20:27:11 -0500

    property binding

--- angularApp/src/app/server/server.component.ts
+++ angularApp/src/app/server/server.component.ts
@@ -1,9 +1,31 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
-export class ServerComponent {
-  
+export class ServerComponent implements OnInit{
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  allowNewServer = false;
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getServerId() {
+    return this.serverId
+  }
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
+
+  ngOnInit() {
+
+  }
+
 }
